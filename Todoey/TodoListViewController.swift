9eda0a9e855c4213c9cf062dc0bbad8d820f3c12
Swanand Deshpande <swananddesh15@gmail.com>
2021-06-28 18:07:48 +0530

    Default ViewController removed and TableViewController is added to its place. Also NavigationController is added.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoListViewController: UITableViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
