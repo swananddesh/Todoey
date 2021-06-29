9a970fb1b020d9b667cef71c590839652ea447f2
Swanand Deshpande <swananddesh15@gmail.com>
2021-06-29 22:46:32 +0530

    Added TableView DataSource and Delegate method with dummy data to render inside TableView.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,10 +9,41 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
+    let itemArray = ["Buy Milk", "Buy Maggie", "Buy Fruits"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    //MARK - TableView DataSource Methods
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    
+    // MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(itemArray[indexPath.row])")
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark // Add checkmark as an accessory to selected row.
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true) // Row highlight.
     }
 
 }
